3fd08c1be5e1b46ea95b3e88a0759344a82921ca
David Stalnaker <david.stalnaker@gmail.com>
2011-04-28 13:57:26 -0400

    added markdown support for posts

--- controller.js
+++ controller.js
@@ -1,7 +1,12 @@
+var markdown = require('node-markdown').Markdown;
+
 module.exports = function(model) {
 	return {
 		home: function(req, res) {
 			model.getPosts(function(err, posts) {
+				posts.forEach(function(elem, i) {
+					elem.renderedContent = markdown(elem.content, true);
+				});
 				res.render('home', { posts: posts });
 			});
 		},
@@ -19,6 +24,7 @@ module.exports = function(model) {
 		viewPost: function(req, res, next) {
 			model.getPost(req.params.slug, function(err, post) {
 				if(post){
+					post.renderedContent = markdown(post.content, true);
 					res.render('viewPost', {post: post});
 				}
 				else {
