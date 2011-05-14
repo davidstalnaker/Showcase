a477fc4161c9d27cf67bde6eb2bc021d6a6394b3
David Stalnaker <david.stalnaker@gmail.com>
2011-05-14 14:22:21 -0400

    turned blog and auth into modules and made them configurable

--- examples/server.js
+++ examples/server.js
@@ -1,3 +1,9 @@
-app = require('../lib/index').app;
+var cms = require('../lib/index');
+var app = cms.app;
+var blog = cms.blog({}, {});
+var auth = cms.auth({}, {});
+
+app.addCMSModule(auth, '');
+app.addCMSModule(blog, '');
 
 app.listen(9001);
