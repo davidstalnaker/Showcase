32ea7a78c1c1d3af3d55034c1e07cd8f6326b282
David Stalnaker <david.stalnaker@gmail.com>
2011-05-25 23:02:33 -0400

    made layout.html configurable

--- examples/server.js
+++ examples/server.js
@@ -3,6 +3,10 @@ var app = cms.app;
 var blog = cms.blog({}, {});
 var auth = cms.auth({}, {});
 
+app.set('view options', {
+	layout: __dirname + '/views/layout'
+});
+
 app.addCMSModule(auth, '');
 app.addCMSModule(blog, '');
 
