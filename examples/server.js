3deb672132f953333504065461e0c7c49b259781
Max Bogue <max@maxbogue.com>
2011-09-01 12:00:47 -0400

    Massive rewrite of Showcase main and the auth module.  A new inheritance system cleans up the code considerably and makes using Showcase much easier.

--- examples/server.js
+++ examples/server.js
@@ -1,13 +1,8 @@
-var showcase = require('../lib/showcase');
-var app = showcase.app;
-var blog = showcase.blog({}, {});
-var auth = showcase.auth({}, {});
+var showcase = require('../lib/showcase')();
+var app = showcase.server;
 
 app.set('view options', {
 	layout: __dirname + '/views/layout'
 });
-
-app.addCMSModule(auth, '');
-app.addCMSModule(blog, '');
 
 app.listen(9001);
